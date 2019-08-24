dc1a9abbe18e028445f4fa667dd98f54d9426d8d
iCrawl <icrawltogo@gmail.com>
2019-08-24 22:24:35 +0200

    fix: properly resolve path

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { resolve } from 'path';
 import { exec } from '@actions/exec';
 import { which } from '@actions/io';
 import { setFailed } from '@actions/core';
 
 async function run() {
 	try {
-		await exec(await which('bash', true), ['src/deploy.sh'], { cwd: process.cwd() });
+		await exec(await which('bash', true), ['src/deploy.sh'], { cwd: resolve(__dirname, '..') });
 	} catch (error) {
 		setFailed(error.message);
 	}
