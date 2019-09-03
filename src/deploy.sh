f9dcaa9c81aa4fb643094e0004f6ea2f6c6ee570
iCrawl <icrawltogo@gmail.com>
2019-09-03 15:48:58 +0200

    refactor: split off docs

--- src/deploy.sh
+++ src/deploy.sh
@@ -4,11 +4,10 @@ set -eo pipefail
 
 cd $GITHUB_WORKSPACE
 
-echo "# Run the build"
+echo "::[notice] # Run the build"
 npm run docs
-NODE_ENV=production npm run build:browser
 
-echo "# Initialise some useful variables"
+# Initialise some useful variables
 REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 BRANCH_OR_TAG=`awk -F/ '{print $2}' <<< $GITHUB_REF`
 CURRENT_BRANCH=`awk -F/ '{print $NF}' <<< $GITHUB_REF`
@@ -19,38 +18,17 @@ else
   SOURCE_TYPE="tag"
 fi
 
-echo "# Checkout the repo in the target branch so we can build docs and push to it"
+echo "::[notice] # Checkout the repo in the target branch so we can build docs and push to it"
 TARGET_BRANCH="docs"
 git clone $REPO out -b $TARGET_BRANCH
 
-echo "# Move the generated JSON file to the newly-checked-out repo, to be committed and pushed"
-mv docs/docs.json out/$CURRENT_BRANCH.json
-
-echo "# Commit and push"
 cd out
 git pull
+echo "::[notice] # Move the generated JSON file to the newly-checked-out repo, to be committed and pushed"
+mv ../docs/docs.json $CURRENT_BRANCH.json
+echo "::[notice] # Commit and push"
 git add .
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git commit -m "Docs build for ${SOURCE_TYPE} ${CURRENT_BRANCH}: ${GITHUB_SHA}" || true
-git push origin $TARGET_BRANCH
-
-echo "# Clean up..."
-cd ..
-rm -rf out
-
-echo "# ...then do the same once more for the webpack"
-TARGET_BRANCH="webpack"
-git clone $REPO out -b $TARGET_BRANCH
-
-echo "# Move the generated webpack over"
-mv webpack/discord.min.js out/discord.$CURRENT_BRANCH.min.js
-
-echo "# Commit and push"
-cd out
-git pull
-git add .
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git commit -m "Webpack build for ${SOURCE_TYPE} ${CURRENT_BRANCH}: ${GITHUB_SHA}" || true
 git push origin $TARGET_BRANCH
